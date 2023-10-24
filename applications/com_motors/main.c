04e866b1e314e83da4effcd8501d8301b7a1a40f
Walthzer <s.v.feitsma@gmail.com>
2023-10-24 23:39:36 +0200

    update demo

--- applications/com_motors/main.c
+++ applications/com_motors/main.c
@@ -1,6 +1,14 @@
  /*
  *  TU/e 5WEC0::PYNQ RYB Communication Demonstration. Motors Drivers module.
  *
+ *  Interface:
+ *  Switch 1 -> Exits the main loop
+ *  Switch 0: Changes our value selection
+ *  Button 3: Resets the value
+ *  Button 2: Modify increase/decrease with a factor of ten;
+ *  Button 1: Increases value
+ *  Button 0: Decreases value
+ * 
  *  Written by: Walthzer
  * 
  */
@@ -34,33 +42,34 @@ int main(void) {
   buttons_init();
 
   int state = 0;
-  int counter = 0;
+  int counters[] = {0,0};
   int button0 = get_button_state(0);
   int button1 = get_button_state(1);
   while (get_switch_state(1))
   {
+    int *counter = &counters[get_switch_state(0)];
     //Dynamic Values
     state = get_button_state(0);
     if(state != button0)
     {
-      counter-= state + 10*get_button_state(2);
+      *counter-= state + 10*get_button_state(2);
       button0 = state;
     }
     state = get_button_state(1);
     if(state != button1)
     {
-      counter+= state + 10*get_button_state(2);
+      *counter+= state + 10*get_button_state(2);
       button1 = state;
     }
     //Reset counter
     if(get_button_state(3))
-      counter = 0;
+      *counter = 0;
 
     uint32_t data = 0;
     bool is_new = com_get(&com, MOTOR, &data);
 
-    ui_rprintf(&ui, 4, "%qFrequency: %q%d", RGB_BLUE, RGB_RED, get_switch_state(0));
-    ui_rprintf(&ui, 5, "%w%qAmplitude: %w%d", RGB_BLACK, RGB_BLUE, RGB_GREEN, counter);
+    ui_rprintf(&ui, 4, "%qFrequency: %q%d", RGB_BLUE, RGB_RED, counters[0]);
+    ui_rprintf(&ui, 5, "%qAmplitude: %q%d", RGB_BLUE, RGB_YELLOW, counters[1]);
 
     ui_draw(&ui);
     com_run(&com);

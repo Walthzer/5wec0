78fb82d02e3a1204d567f6bfc64975826730ed31
Walthzer <s.v.feitsma@gmail.com>
2023-12-06 16:17:15 +0100

    glitch filter single pulse

--- applications/mft_heartbeat/main.c
+++ applications/mft_heartbeat/main.c
@@ -12,14 +12,17 @@
  *  Written by: Walthzer
  * 
  */
+
+#include <unistd.h>
 #include <libpynq.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 #include <libui.h>
 #include <libcom.h>
-
 #include <pthread.h>
 #include <stdatomic.h>
+
+//#define DEBUG
 
 #define MIN(a,b) a > b ? b : a
 #define MAX(a,b) a > b ? a : b
@@ -28,7 +31,11 @@
 #define TEST_PIN IO_AR7
 
 #define PULSE_COUNT 2
-#define MIN_PERIOD_MS 5
+#define MIN_PERIOD_MS 100
+#define GLITCH_PERIOD 5
+#define MIN_READINGS 4
+
+#define FREE_PTR(ptr) free(ptr); ptr = NULL
 
 struct thread_arg
 {
@@ -36,9 +43,17 @@ struct thread_arg
   com_t* com;
 };
 
+enum SIGNAL_EVENT {NONE, RISE, FALL}; 
+
+typedef struct _pulse_event_
+{
+  int type;
+  struct timeval time;
+} Event;
+
 typedef struct _pulse_
 {
-  clock_t rise, fall;
+  Event events[2];
   int valid;
 } Pulse;
 
@@ -46,9 +61,10 @@ typedef struct _signal_
 {
   Pulse pulses[PULSE_COUNT]; //Falling Edge timestamps
   int state, next_pulse;
+  int period;
+  int readings[MIN_READINGS];
+  int curr_reading;
 } Signal;
-
-enum SIGNAL_EVENT {NONE, RISE, FALL}; 
 
 //Globals
 //FLAGS
@@ -56,9 +72,8 @@ enum CRADLE_MODES {SQUARE, ECG};
 static int Cradle_Mode = -1; 
 
 //PERIOD
-atomic_int_fast16_t period_atom;
+atomic_int period_atom;
 atomic_bool output_kalive;
-int16_t period;
 
 //Get the truth value of the sensor
 bool get_sensor_state()
@@ -68,18 +83,49 @@ bool get_sensor_state()
   return state > 2000;
 }
 
+int median_of_three(int a, int b, int c) {
+    if ((a > b) ^ (a > c)) 
+        return a;
+    else if ((b < a) ^ (b < c)) 
+        return b;
+    else
+        return c;
+}
+
 //Calculate ms period from rise and fall stamps
 //Results is always positive
-long double period_ms(clock_t a, clock_t b)
+int signed_time_diff_ms(struct timeval *a, struct timeval *b)
 {
-  return ((long double)((a > b ? a - b : b - a)*1000 / (clock_t)(CLOCKS_PER_SEC)));
+  return ((a->tv_sec - b->tv_sec) + 1e-6 * (a->tv_usec - b->tv_usec)) * 1000.0f;
+}
+
+
+int time_diff_ms(struct timeval *a, struct timeval *b) {
+
+  int delta = signed_time_diff_ms(a, b);
+  if (delta < 0)
+    return -delta;
+  return delta;
+}
+
+void clear_pulse_event(Event* event)
+{
+  event->type = NONE;
+  event->time = (struct timeval){0, 0};
+}
+
+void clear_pulse(Pulse* pulse)
+{
+  pulse->valid = 0;
+  clear_pulse_event(&pulse->events[0]);
+  clear_pulse_event(&pulse->events[1]);
 }
 
 void process_input(Signal *signal)
 {
   int event = NONE;
-  clock_t t_curr = clock();
-
+  struct timeval p_t_curr = {0, 0};
+  gettimeofday(&p_t_curr, NULL);
   
   if(!get_sensor_state())
   { 
@@ -113,34 +159,83 @@ void process_input(Signal *signal)
   //SQUARE MODE
   if(Cradle_Mode == SQUARE)
   {
-    Pulse *pulse = &signal->pulses[signal->next_pulse];
+    //Signal is marked valid by glitch timeout
+    /*Signal can be discarded when 
+      * -> period
+      or
+      * -> delta between last event and a next event
+      is less then MIN_PERIOD_MS
+    */
+    //Hard coded pulse -> square only needs one
+    Pulse *pulse = &signal->pulses[0];
+    int delta_ms;
 
-    //No event -> Check for glitch timeout
+    //No event
+    //Check for glitch timeout
+    //Check for invalid period
     if(event == NONE)
     {
+      if(pulse->events[0].type == NONE || pulse->events[1].type == NONE)
+        return;
+
+      //Is delta between now and last event is greater then GLITCH_PERIOD
+      if(time_diff_ms(&p_t_curr, &pulse->events[1].time) >= GLITCH_PERIOD)
+      {
+        //Is period less then MIN_PERIOD_MS
+        delta_ms = time_diff_ms(&pulse->events[0].time, &pulse->events[1].time);
+        if(delta_ms < MIN_PERIOD_MS)
+        {
+          //Discard pulse
+          printf("--Clear Pulse\n");
+          clear_pulse(pulse);
+          return;
+        }
+        
+        clear_pulse(pulse);
+        signal->period = delta_ms;
+      }
       return;
-    } 
-
-    //Complete and invalid signal?
-    if((!pulse->valid) && pulse->fall != 0 && pulse->rise != 0)
-    {
-
     }
 
-    //printf("Event: %d\n", event);
+
+
+    //Check delta between current event and last event
+    //to filter glitches
+    //Only try to filter invalid signals
+    if(pulse->events[0].type != NONE && pulse->events[1].type != NONE)
+    {
+      //Is delta between now and last event less then MIN_PERIOD_MS
+      if(time_diff_ms(&p_t_curr, &pulse->events[1].time) < GLITCH_PERIOD)
+      {
+        //clear this glitch from signal
+        printf("--Clear Event\n");
+        clear_pulse_event(&pulse->events[1]);
+        return;
+      }
+    }
     //Event is good
-    //Store time in pulse
+    //Store event -> 0 or 1 depending on what is filled
+    int idx_event = pulse->events[0].type != NONE;
+    pulse->events[idx_event].type = event;
+    pulse->events[idx_event].time = p_t_curr;
+
+    #ifdef DEBUG
     if(event == RISE)
     {
-      signal->pulses[signal->next_pulse].rise = t_curr;
-      gpio_set_level(TEST_PIN, GPIO_LEVEL_HIGH);
+      printf("RISE");
     }
     
     if(event == FALL)
     {
-      signal->pulses[signal->next_pulse].fall = t_curr;
-      gpio_set_level(TEST_PIN, GPIO_LEVEL_LOW);
+      printf("FALL");
     }
+    #endif
+
+    if(pulse->events[0].type != NONE && pulse->events[1].type != NONE)
+    {
+      printf("\nPeriod: %d", time_diff_ms(&pulse->events[0].time, &pulse->events[1].time));
+    }
+
   }
 }
 
@@ -154,13 +249,24 @@ void* fnc_output_thread(void* arg)
     //com_put(&com, HEARTBEAT, data.bpm);
     //com_get(&com, HEARTBEAT, &data.bpm);
     
+    double period;
+    struct timespec t_sleep = {0, 10000};
+    int mode = Cradle_Mode;
+
     while(atomic_load(&output_kalive))
     {
-      double period_local = atomic_load(&period_atom);
-      double frequency = 1000.0 / period_local;
-      int bpm = 60 * (500.0 / period_local); //150ms -> 200  // 1000ms -> 30  //500ms -> 60
+      double new_period = atomic_load(&period_atom);
+      if(new_period == period && Cradle_Mode == mode)
+      {
+        nanosleep(&t_sleep, NULL); //sleep in microseconds
+        continue;
+      }
+      mode = Cradle_Mode;
+      period = new_period;
+      double frequency = 500.0 / period;
+      int bpm = 60 * (500.0 / period); //150ms -> 200  // 1000ms -> 30  //500ms -> 60
 
-      printf("---------PERIOD: %f ---------\n", period_local);
+      printf("---------PERIOD: %f ---------\n", period);
       ui_rprintf(ui, 3, "%qMode: %q%s", RGB_ORANGE, RGB_RED, Cradle_Mode ? "ECG" : "Square");
       ui_rprintf(ui, 6, "%qFrequency: %q%f", RGB_PURPLE, RGB_GREEN, frequency);
       ui_rprintf(ui, 7, "%qBPM:[60-240]: %q%d", RGB_PURPLE, RGB_GREEN, bpm);
@@ -214,9 +320,18 @@ int main(void) {
   Signal signal;
   signal.state = get_sensor_state();
   signal.next_pulse = 0;
+  signal.period = 0;
+  signal.curr_reading = 0;
+  for (int i = 0; i < MIN_READINGS; i++)
+  {
+      signal.readings[i] = 0;
+  }
+
   for (int i = 0; i < PULSE_COUNT; i++)
   {
-    signal.pulses[i] = (Pulse){0, 0, 0};
+    signal.pulses[i].valid = 0;
+    signal.pulses[0].events[0] = (Event){NONE, {0,0}};
+    signal.pulses[0].events[1] = (Event){NONE, {0,0}};
   }
   //---END Signal---
   //---END Vars---
@@ -224,7 +339,7 @@ int main(void) {
   //Threading
   atomic_init(&period_atom, 50);
   atomic_init(&output_kalive, true);
-  period = atomic_load(&period_atom);
+  signal.period = atomic_load(&period_atom);
   struct thread_arg thrd_args = {&ui, &com};
   pthread_t output_thread;
   pthread_create(&output_thread, NULL, &fnc_output_thread, &thrd_args);
@@ -236,27 +351,23 @@ int main(void) {
 
     process_input(&signal);
 
-    //Find valid pulse
-    Pulse *valid_pulse = NULL;
-    for (int i = 0; i < PULSE_COUNT; i++)
+    //Mode depended pulse handeling
+    if(Cradle_Mode == ECG)
     {
-      if(signal.pulses[i].rise > 0.0f && signal.pulses[i].fall > 0.0f)
-      {
-        valid_pulse = &signal.pulses[i];
-        valid_pulse->valid = 0;
-        break;
-      }
+      continue;
     }
 
-    if(valid_pulse != NULL)
+    if(Cradle_Mode == SQUARE)
     {
-      int delta_ms = (int)period_ms(valid_pulse->rise, valid_pulse->fall);
-      if(delta_ms != period)
+      //Await READINGS for average reading
+      if(signal.period != atomic_load(&period_atom))
       {
-        period = delta_ms;
-        atomic_store(&period_atom, period);
-      };
+        printf("---------PERIOD: %d ---------\n", signal.period);
+        fflush(NULL);
+        atomic_store(&period_atom, signal.period);
+      }
       
+      continue;
     }
   }
 

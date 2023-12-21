a0e8a4c549c856604ab60b6bfd5a6f390471bf2c
Walthzer <s.v.feitsma@gmail.com>
2023-12-21 16:27:09 +0100

    pwm inversion fix

--- applications/mft_motors/main.c
+++ applications/mft_motors/main.c
@@ -49,7 +49,7 @@ int dutycycle_period(int index)
 {
   float decimal = 0.01 * dutycycle_percent(index);
 
-  return (PWM_PERIOD * decimal);
+  return (PWM_PERIOD - (PWM_PERIOD * decimal));
 }
 
 
@@ -78,8 +78,8 @@ int main(void) {
 
   //Init PWM
   switchbox_init();
-  switchbox_set_pin(IO_AR7, SWB_PWM0);
-  switchbox_set_pin(IO_AR8, SWB_PWM1);
+  switchbox_set_pin(IO_AR5, SWB_PWM0);
+  switchbox_set_pin(IO_AR4, SWB_PWM1);
   pwm_init(PWM0, PWM_PERIOD); //1Khz -> nanoseconds
   pwm_init(PWM1, PWM_PERIOD); //1Khz -> nanoseconds
 
